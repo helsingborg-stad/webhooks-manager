ee4c01733c3daeef3aba3f1ff1ba1d2f06493483
Thor Brink <thor.brink@helsingborg.se>
2023-11-28 09:14:42 +0000

    feat: add field group

---
+++ source/php/AcfFields/php/webhooks.php
@@ -0,0 +1,48 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_6565af01867da',
+    'title' => __('Webhooks', 'webhooks-manager'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_6565af01d3812',
+            'label' => '',
+            'name' => '',
+            'aria-label' => '',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'maxlength' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'options_page',
+                'operator' => '==',
+                'value' => 'webhooks-manager',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+));
+}
