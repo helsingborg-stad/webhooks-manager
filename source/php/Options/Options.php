5cc723412d195cfeb05479718f6d766af5863d5b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-04-11 10:18:51 +0200

    feat: adds options to action selector for cronjobs registered.

--- source/php/Options/Options.php
+++ source/php/Options/Options.php
@@ -43,14 +43,31 @@ class Options implements OptionsInterface
     }
 
     /**
+     * An array of HTTP methods supported by the plugin.
+     *
+     * @return array The supported HTTP methods.
+     */
+    public function getTypeLabels(): object
+    {
+        return (object) [
+            'cron' => __("Cron:"),
+            'post' => __("Post:")
+        ];
+    }
+
+    /**
      * Retrieves the available post actions.
      *
      * @return array The available post actions.
      */
     private function getPostActions(): array
     {
+        $avabilePostActions = []; 
+        foreach(self::DEFAULT_ACTIONS as $action) {
+            $avabilePostActions[$action] = $this->getTypeLabels()->post . " " . $action;
+        }
         return apply_filters('WebhooksManager\Options\getPostActions', 
-            self::DEFAULT_ACTIONS
+            $avabilePostActions
         );
     }
 
@@ -61,8 +78,25 @@ class Options implements OptionsInterface
      */
     private function getCronActions(): array
     {
+        $cron = get_option('cron', []);
+        $avabileCronActions = [];
+
+        if(!empty($cron) && is_countable($cron)) {
+            foreach ($cron as $value) {
+                if(!is_countable($value)) {
+                    continue;
+                }
+                foreach($value as $action => $cronItem) {
+                    $cronItem = array_pop($cronItem);
+                    if($cronItem['schedule'] !== false) {
+                        $avabileCronActions[$action] = $this->getTypeLabels()->cron . " " . $action;
+                    }
+                }
+            }
+        }
+
         return apply_filters('WebhooksManager\Options\getCronActions', 
-            []
+            $avabileCronActions
         );
     }
 }
