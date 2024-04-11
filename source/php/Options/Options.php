923a28b71d445384bffcf0c00a03ab426e66d6dd
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-04-11 08:40:29 +0200

    feat: add function to fetch cronjobs and merge them with post hooks.

--- source/php/Options/Options.php
+++ source/php/Options/Options.php
@@ -35,6 +35,34 @@ class Options implements OptionsInterface
      */
     public function getActions(): array
     {
-        return apply_filters('WebhooksManager\Options\getActions', self::DEFAULT_ACTIONS);
+        return apply_filters('WebhooksManager\Options\getActions', array_merge(
+                $this->getPostActions(),
+                $this->getCronActions()
+            )
+        );
+    }
+
+    /**
+     * Retrieves the available post actions.
+     *
+     * @return array The available post actions.
+     */
+    private function getPostActions(): array
+    {
+        return apply_filters('WebhooksManager\Options\getPostActions', 
+            self::DEFAULT_ACTIONS
+        );
+    }
+
+    /**
+     * Retrieves the available cron actions.
+     *
+     * @return array The available cron actions.
+     */
+    private function getCronActions(): array
+    {
+        return apply_filters('WebhooksManager\Options\getCronActions', 
+            []
+        );
     }
 }
