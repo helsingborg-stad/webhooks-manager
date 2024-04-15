e8d25e42a0e106f86a937f2aa78acbb49a39852a
Thor Brink <thor.brink@helsingborg.se>
2024-04-15 10:33:17 +0000

    refactor: remove unused test framework patchwork

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,5 @@
 // First we need to load the composer autoloader, so we can use WP Mock
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-// Bootstrap Patchwork
-WP_Mock::setUsePatchwork(true);
-
 // Bootstrap WP_Mock to initialize built-in features
-// WP_Mock::activateStrictMode();
 WP_Mock::bootstrap();
